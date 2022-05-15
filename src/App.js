7b4fc6cefd3131654e72887b26eac69af7cfc10c
Martin <martingauna2000@gmail.com>
2022-05-14 22:49:34 -0300

    añadiendo card items

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 
 import './App.css';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 import { Navbar } from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1>Aqui arriba va a estar el Navbar</h1>
+      <ItemListContainer />
     </div>
   );
 }
