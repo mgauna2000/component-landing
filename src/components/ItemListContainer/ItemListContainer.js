cc4729817374296ecdadd704155bc7e80bda9e5a
Martin <martingauna2000@gmail.com>
2022-05-15 22:21:44 -0300

    grid card responsive

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -5,7 +5,7 @@ const ItemListContainer = () => {
   return (
     <>
       <div class="container">
-        <div class="row g-4">
+        <div class="row row-cols-1 row-cols-md-3 mt-4 g-4">
           <div class="col">
             <div class="card">
               <img src="./manga1.webp" class="card-img-top" alt="..." />
@@ -28,6 +28,42 @@ const ItemListContainer = () => {
                   This is a longer card with supporting text below as a natural
                   lead-in to additional content. This content is a little bit
                   longer.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img src="./manga3.webp" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img src="./manga3.webp" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col">
+            <div class="card">
+              <img src="./manga3.webp" class="card-img-top" alt="..." />
+              <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">
+                  This is a longer card with supporting text below as a natural
+                  lead-in to additional content.
                 </p>
               </div>
             </div>
